a6b428bef83447888ec5b3688ca7b90b9005da3e
nvashish123 <nitin.vashi@gmail.com>
2019-11-23 22:16:57 +0000

    initial commit

---
+++ app/listS3Function.py
@@ -0,0 +1,9 @@
+import boto3
+
+def lambda_handler(event, context):
+
+    s3 = boto3.client('s3')
+    response = s3.list_buckets()
+    buckets = [bucket['Name'] for bucket in response['Buckets']]
+    print("Bucket List: %s" % buckets)
+    
